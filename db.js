1928c4cb60bc88f23623647bab6f94561dac5fac
Zafarullah Shafiq <zafarullahshafiqee@gmail.com>
2026-04-23 22:02:22 +0500

    editDB

--- db.js
+++ db.js
@@ -1,11 +1,14 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'myapp',
-  password: '12345',
+  user: 'zafar',
+  host: 'zafarullah.ctqsgygqc3t7.eu-north-1.rds.amazonaws.com',
+  database: 'myDB',
+  password: 'zafarullah123',
   port: 5432,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
